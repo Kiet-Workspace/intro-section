3145b5dab98258bf081ec80b9362dab775faa20c
Kiet-Workspace <117105104+Kiet-Workspace@users.noreply.github.com>
2023-04-02 16:46:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ var closeNavIcon = document.querySelector(".close-nav");
 
 
 function showNav() {
-    // navBox.style.display = "block";
-    navBox.classList.add("show");
+    navBox.style.display = "block";
+    // navBox.classList.add("show");
 }
 
 function hideNav() {
-    // navBox.style.display = "none";
-    navBox.classList.remove("show");
+    navBox.style.display = "none";
+    // navBox.classList.remove("show");
 }
 
 openNavIcon.addEventListener("click", showNav);
